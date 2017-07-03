60102951adc6dbde1795af4ac1355392643ef466
Matt182 <mattspring0@gmail.com>
2017-07-03 19:24:32 +0000

    test me 2

--- ex1.c
+++ ex1.c
@@ -3,8 +3,7 @@
 /* This is a comment */
 int main(int argc, char *argv[])
 {
-	int distance = 100;
-	
+	int distance = 100;	
 	// this also a comment
 	printf("You are %d miles away.\n", distance);
 
