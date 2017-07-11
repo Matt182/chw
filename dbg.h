b3ee58918c08c0ed2355d59e16416d57e4fe5216
Matt182 <mattspring0@gmail.com>
2017-07-11 19:56:25 +0000

    ex24

--- dbg.h
+++ dbg.h
@@ -15,14 +15,14 @@
 #define clean_errno() (errno == 0 ? "None" : strerror(errno))
 
 #define log_err(M, ...) fprintf(stderr,\
-		"[ERROR] (%s:%d: errno: %s, function:%S) " M "\n", __FILE__, __LINE__,\
+		"[ERROR] (%s:%d: errno: %s, function:%s) " M "\n", __FILE__, __LINE__,\
 		clean_errno(), ##__VA_ARGS__)
 
 #define log_warn(M, ...) fprintf(stderr,\
 		"[WARN] (%s:%d: errno: %s) " M "\n",\
 		__FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
 
-#define log_info(M, ...) fprintf(stderr, "[INFO] (%S:%d) " M "\n",\
+#define log_info(M, ...) fprintf(stderr, "[INFO] (%s:%d) " M "\n",\
 		__FILE__, __LINE__, ##__VA_ARGS__)
 
 #define check(A, M, ...) if(!(A)) {\
